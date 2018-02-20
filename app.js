1a2ec815863cfe6fb56880ae49a5b52396b72623
rajusingh <raju.singh41@gmail.com>
2018-02-20 22:22:09 +0530

    Add Views

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express =require('express');
-var bodyParser=require('body-parser');
-var dbConfig=require('./config/database.config');
-var mongoose=require('mongoose');
-var app =express();
-mongoose.Promise=global.Promise;
+var express = require('express');
+var bodyParser = require('body-parser');
+var dbConfig = require('./config/database.config');
+var mongoose = require('mongoose');
+var employees=require('./routes/employees');
+var app = express();
+mongoose.Promise = global.Promise;
 
 mongoose.connect(dbConfig.url)
-.then(() =>  console.log('connection succesful'))
-.catch((err) => console.error(err));
+    .then(() => console.log('connection succesful'))
+    .catch((err) => console.error(err));
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.get('/',(req,res)=>{
-    res.json({'message':'Welcome to my Demo App'});
+app.get('/', (req, res) => {
+    res.json({ 'message': 'Welcome to my Demo App' });
 })
 
+app.use('/employess',employees);
 
 // https://www.djamware.com/post/58b27ce080aca72c54645983/how-to-create-nodejs-expressjs-and-mongodb-crud-web-application
 app.listen(3000);
---
+++ routes/employees.js
@@ -0,0 +1,33 @@
+
+var express = require('express');
+var router = express.Router();
+
+var employee = require('../controllers/EmployeeController');
+
+
+// Get All Employee
+
+router.get('/', employee.list);
+
+// Get Single Employee By id
+
+router.get('/show/:id', employee.show);
+
+// Save Employee
+
+router.post('/save', employee.save);
+
+// Edit employee 
+
+router.get('/edit/:id', employee.edit);
+
+// Edit Update
+
+router.post('/update/:id', employee.update);
+
+// Delete Employee
+
+router.post('/delete/:id', employee.delete);
+
+
+module.exports = router;
